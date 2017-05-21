5dab154ae70698ed50de72e4edf80d7c41486af2
Jerry <zoj3zoj3@gmail.com>
2017-05-20 18:36:59 -0700

    Update water_level_indicator
    When water is more than 20%, no LED will be light.
    
    If water is below 20% but above 0%, one LED will be light.
    
    If water is equal to 0 %, 2 LEDs will be light.
    
    Signed-off-by: Jerry Hung

--- WaterLevel/WaterLevel.ino
+++ WaterLevel/WaterLevel.ino
@@ -1,20 +1,39 @@
-int ledPin = 13; // LED connected to digital pin 13
-int inPin = 7;   // pushbutton connected to digital pin 7
-int val = 0;     // variable to store the read value
+int ledPin1 = 6; // LED connected to digital pin 13
+int ledPin2 = 7; // LED connected to digital pin 13
+int level_1 = 3; // 20% level 
+int level_2 = 4; // 0% level 
 
+int on_1 = 0;
+int on_2 = 0;
 void setup()
 {
   Serial.begin(9600);
   delay(500);//Delay to let system boot
-  //pinMode(ledPin, OUTPUT);      // sets the digital pin 13 as output
-  pinMode(inPin, INPUT);      // sets the digital pin 7 as input
-  Serial.println("fuck");
+  pinMode(ledPin1, OUTPUT);      // sets the digital pin 13 as output
+  pinMode(ledPin2, OUTPUT);      // sets the digital pin 13 as output
+  pinMode(level_1, INPUT);      // sets the digital pin 7 as input
+  pinMode(level_2, INPUT);      // sets the digital pin 7 as input
 }
-
+void water_level_status(){
+  on_1 = digitalRead(level_1);   // read the input pin
+  on_2 = digitalRead(level_2);   // read the input pin
+  Serial.println(on_1);
+  Serial.println(on_2);
+  if(on_1 == 0){
+      digitalWrite(ledPin1,HIGH);
+  }else{
+      digitalWrite(ledPin1,LOW);
+  }
+  if(on_2 == 0){
+      digitalWrite(ledPin2,HIGH);
+  }else{
+      digitalWrite(ledPin2,LOW);
+  }
+  delay(1000);
+  
+}
 void loop()
 {
-  val = digitalRead(inPin);   // read the input pin
-  //digitalWrite(ledPin, val);    // sets the LED to the button's value
-  Serial.println(val);
-  delay(1000);
+  water_level_status();
+  
 }
