ecfb4ef36bc8c2c8a45ad20869082c771d7546e2
Jerry <zoj3zoj3@gmail.com>
2017-05-20 17:57:36 -0700

    Updating the watering system
    Optimize the soil moisture sensors' reading. Also, the system is able to
    detect 2 sensors and their status( including connection status)
    
    Note:
    
    The timing function(no manual watering withing 5 seconds) has not been
    implemented
    
    Signed-off-by: Jerry Hung

--- watering_system/watering_system.ino
+++ watering_system/watering_system.ino
@@ -1,5 +1,16 @@
+#include <Time.h>
+
 
  #define CH1 7   // Connect Digital Pin 7 on Arduino to CH1 on Relay Module
+int moisture_1 = 0;
+int moisture_2 = 0;
+int sensor_1 = 0;// temp decision, will change input pin later
+int sensor_2 = 1;
+int moisture_avg = 0;
+
+boolean sensor_flag_1 = true;
+boolean sensor_flag_2 = true;
+boolean watering = false;
 
 void setup() {
 // initialize serial communication at 9600 bits per second:
@@ -13,29 +24,72 @@ digitalWrite(CH1,LOW);
 delay(2000); //Wait 2 seconds before starting sequence
 }
 
+void reading_moisture_sensors(void){
+  moisture_1 = analogRead(sensor_1);
+  moisture_2 = analogRead(sensor_2);
+  Serial.print(moisture_1);
+  Serial.print("above is sensor 1\n");
+  Serial.print(moisture_2);
+  Serial.print("above is sensor 2\n");
+  moisture_avg = (moisture_1 + moisture_2)/2;
+  }
+void update_sensor_data(void){
+  reading_moisture_sensors();
+}
+
+void moisture_status_state (){
+  if(moisture_1 < 20 ){
+    Serial.print("\tSoil sensor 1 is not connect to soil\n");
+    sensor_flag_1 = false;
+    watering = false;
+  } else if(moisture_2 < 20 ){
+    Serial.print("\tSoil sensor 2 is not connect to soil\n");
+    sensor_flag_2 = false;
+    watering = false;
+  }
+  else if(moisture_avg < 200 && moisture_avg > 20){
+   Serial.print(moisture_avg);
+   Serial.print("\tWatering \n");
+   watering = true;  
+  }  else{
+    digitalWrite(CH1, LOW); 
+    if(moisture_avg > 800){
+      Serial.print("\tSoil is wet, not need to water\n");
+      watering = false;
+    }else{
+      Serial.print("\tSoil is fine!!!!\n");
+      watering = false;
+    }
+  }
+}
+
+void pumping (){
+
+  if (!sensor_flag_1 || !sensor_flag_2){
+      Serial.print("\tOne of the Soil sensors is not connect to soil\n");
+  }
+
+  if (watering){
+    if (false){
+      digitalWrite(CH1, LOW);
+        /*This part is for timing issue*/
+  
+         /**-------------------------------------*/
+      }else {
+     digitalWrite(CH1, HIGH);        
+    }
+  }else {
+     digitalWrite(CH1, LOW);
+    }
+  sensor_flag_1 = true;
+  sensor_flag_2 = true;
+  watering = false;
+}
 void loop() {
 // read the input on analog pin 0:
-int sensorValue = analogRead(A0);
-int pw_delay = 5000;
-
-if(sensorValue < 200 && sensorValue > 20){
-  digitalWrite(CH1, HIGH);
-  Serial.print(sensorValue);
-  Serial.print("\tWatering \n");
-}else{
-  digitalWrite(CH1, LOW);
-  Serial.print(sensorValue);
-    
-  if(sensorValue > 800){
-    Serial.print("\tSoil is wet, not need to water\n");
-  }else if(sensorValue < 20){
-    Serial.print("\tSoil sensor is not connect to soil\n");
-  }else{
-    Serial.print("\tSoil is fine!!!!\n");
-  }
-  
+  update_sensor_data();
+  moisture_status_state();
+  pumping();
+  delay(1000);
 }
 
-delay(5000);
-
-}
