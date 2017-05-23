5ab1874a71f073e80ee7f9566c06a25666dd3f2c
Jerry <zoj3zoj3@gmail.com>
2017-05-22 22:02:38 -0700

    Seperate status and action to different function
    Signed-off-by: Jerry Hung

--- Light/Light/Light.ino
+++ Light/Light/Light.ino
@@ -53,23 +53,24 @@ void reading_light_sensors (void){
 void update_sensor_data (void){
   reading_light_sensors();
 }
-void light (boolean red_led, boolean blue_led) {
+
+void light_status (boolean red_led, boolean blue_led) {
 
 
   // Different readings from analog will result in different light intensity. The number will be determine more accurately later
   if (photocell_average < dim_min) {
     Serial.println(" - Dark");
-    digitalWrite(LEDpin1,HIGH);
-    digitalWrite(LEDpin2,HIGH);
+    white_led_1 = true;
+    white_led_2 = true;
      
   } else if (photocell_average < dim_max && photocell_average > dim_min) {
     Serial.println(" - Dim");
-    digitalWrite(LEDpin1,HIGH);
-    digitalWrite(LEDpin2,LOW);
+    white_led_1 = true;
+    white_led_2 = false;
   } else {
     Serial.println(" - Light");
-    digitalWrite(LEDpin1,LOW);
-    digitalWrite(LEDpin2,LOW);
+    white_led_1 = false;
+    white_led_2 = false;
   }
   //This is for the blue and red LED, which will be controll by the app. Using button as the input pretent using app, so we keep sending output
     // 2 variables will be passed into this function to turn on/off blue/red LEDs
@@ -83,12 +84,26 @@ void setup(void) {
   pinMode(LEDpin2, OUTPUT);  
   pinMode(LEDpin3, OUTPUT); 
 }
- 
+ void turn_on_white_light (){
+  if (white_led_1 && white_led_2){
+    digitalWrite(LEDpin1,HIGH);
+    digitalWrite(LEDpin2,HIGH);
+    }
+    else if (white_led_1){
+      digitalWrite(LEDpin1,HIGH);
+      digitalWrite(LEDpin2,LOW);
+      }
+    else{
+      digitalWrite(LEDpin1,LOW);
+      digitalWrite(LEDpin2,LOW);
+      }
+  }
 void loop(void) {
   update_sensor_data();// water level, all sensors data are read in this functions
   //timing issue
-  light(red_led,blue_led);
-
+  light_status(red_led,blue_led);
+  turn_on_white_light();
+  
 
 
 
